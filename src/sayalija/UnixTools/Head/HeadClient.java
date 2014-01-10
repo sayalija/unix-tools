8190d745d59424aa3f6d65ddb580c0f4fa06ce92
sayali <sayali@patanaikya.com>
2014-01-10 13:41:57 +0530

    Both functions are working

--- src/sayalija/UnixTools/Head/HeadClient.java
+++ src/sayalija/UnixTools/Head/HeadClient.java
@@ -1,13 +1,35 @@
 package sayalija.UnixTools.Head;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by sayalija on 1/9/14.
  */
 public class HeadClient {
     public static void main(String[] args) {
-        String str = null;
-        Head client = new Head("s \n a \n y \n a \n l \n i \n  \n j \n a \n g \n t \n a \n p",3);
-        client.getHeader();
-        System.out.println(str);
+        String fileName = args[0];
+        BufferedReader br = null;
+        String text = null;
+        try {
+            String sCurrentLine;
+
+            br = new BufferedReader(new FileReader(fileName));
+
+            while ((sCurrentLine = br.readLine()) != null) {
+                text += "\n" + sCurrentLine;
+            }
+
+        } catch (IOException e) {
+            System.out.println("File not exist");
+        }
+        String str = "";
+        Head client1 = new Head(text);
+        str = client1.getHeader();
+        System.out.println("10 lines--->   \n"+str);
+        Head client2 = new Head(text,3);
+        str = client2.getHeader();
+        System.out.println("3 lines--->   \n"+str);
     }
 }
