94bb1fe47e76b992de22ad4e3266e0c7ff01cfeb
sayali <sayali@patanaikya.com>
2014-01-13 10:32:01 +0530

    Cut working

--- src/sayalija/UnixTools/Cut.java
+++ src/sayalija/UnixTools/Cut.java
@@ -1,5 +1,28 @@
 package sayalija.UnixTools;
 
 public class Cut {
+    String delimiter ;
+    public Cut(){
+        delimiter = " ";
+    }
+    public Cut(String d){
+        delimiter = d;
+    }
+    StringBuilder result = new StringBuilder("");
+
+    public String cutLines(String text, int fieldNumber) {
+        String[] lines;
+        lines = text.split("\n");
+        fieldNumber = fieldNumber - 1;
+        if (!text.contains(delimiter))
+            return text;
+        for (String line : lines) {
+            String[] fields = line.split(delimiter);
+            if (fieldNumber < fields.length)
+                result.append(fields[fieldNumber]).append("\n");
+            else result.append("").append("\n");
+        }
+        return result.toString();
+    }
 
 }
--- src/sayalija/UnixTools/cli/CutClient.java
+++ src/sayalija/UnixTools/cli/CutClient.java
@@ -1,11 +1,21 @@
 package sayalija.UnixTools.cli;
 
+import sayalija.UnixTools.Cut;
 import sayalija.fs.FileSystem;
 
 public class CutClient {
     public static void main(String[] args) {
-        String fileName = args[0];
-        FileSystem fs = new FileSystem();
-        String text = fs.readFile(fileName);
+        try {
+            String fileName = args[0];
+            int fieldNumber = Integer.parseInt(args[1].substring(2, args[1].length()));
+            FileSystem fs = new FileSystem();
+            String text = fs.readFile(fileName);
+            Cut c = new Cut();
+            System.out.println(c.cutLines(text,fieldNumber));
+            c = new Cut(",");
+            System.out.println(c.cutLines(text,fieldNumber));
+        } catch (Exception ex) {
+            System.err.println("Exception" + ex);
+        }
     }
 }
